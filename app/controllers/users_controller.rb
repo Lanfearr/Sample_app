7a658a71f82192c5549b67fbf1644a90b9ef9674
Lanfearr <lanfearr@outlook.com>
2016-01-21 16:09:36 +0000

    user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,29 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(page: params[:page])
     end
   
     def new
-        @user = User.new
+        if signed_in?
+            redirect_to(root_path)
+        else
+            @user = User.new
+        end
     end
 
     def create
-        @user = User.new(params[:user])
-        if @user.save
-            sign_in @user
-            flash[:success] ="Welcome to the Sample App!"
-            redirect_to @user
+        if signed_in?
+            redirect_to(root_path)
         else
-            render 'new'
+            @user = User.new(params[:user])
+            if @user.save
+                sign_in @user
+                flash[:success] ="Welcome to the Sample App!"
+                redirect_to @user
+            else
+                render 'new'
+            end
         end
     end
 
@@ -40,19 +49,18 @@ class UsersController < ApplicationController
     end
 
     def destroy
-        User.find(params[:id]).destroy
-        flash[:success] = "User destroyed."
-        redirect_to users_path
+        @user = User.find(params[:id])
+        if @user.admin?
+            flash[:failure] = "Admins cannot delete themselves."
+            redirect_to users_path
+        else
+            @user.destroy
+            flash[:success] = "User destroyed."
+            redirect_to users_path
+        end
     end
 
     private
-
-        def signed_in_user
-            unless signed_in?
-                store_location
-                redirect_to signin_path, notice: "Please sign in." 
-            end
-        end
 
         def correct_user
             @user = User.find(params[:id])
